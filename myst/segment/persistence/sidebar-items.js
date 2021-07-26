b2648012b686b278bcafc3d22e830cad0bc44eda
sd-buildbot <dev-null@screwdriver.cd>
2021-07-26 17:02:19 +0000

    Update documentation

--- myst/segment/persistence/sidebar-items.js
+++ myst/segment/persistence/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Builder",""],["Loader",""],["RemoteSegmentStore",""]]});
+initSidebarItems({"trait":[["Builder",""],["Loader",""],["RemoteSegmentStore",""],["TimeSegmented","This trait needs to be implemented by any Data structure needing a dynamic duration parameter. i.e Wherever there is a concept of an updating sub-epoch. Ex: Cached versions of data and MystSegment."]]});
