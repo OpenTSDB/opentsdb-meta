e64b262c5bff9e4efbeec8cfb3fc44c7150eeaa2
Ravi kiran Chiruvolu <chiruvol@verizonmedia.com>
2021-08-02 15:23:30 -0700

    pushed fixes

---
+++ create_git_tag.sh
@@ -0,0 +1,14 @@
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null` 
+if [[ $CURRENT_VERSION == '' ]]
+then
+  CURRENT_VERSION='0.1.0'
+fi
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
+VNUM1=${CURRENT_VERSION_PARTS[0]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
+VNUM3=${CURRENT_VERSION_PARTS[2]}
+VNUM3=$((VNUM3+1))
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3" 
+echo "Committing new tag $NEW_TAG"
+git tag $NEW_TAG
+git push --tags
