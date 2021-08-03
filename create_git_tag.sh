dda241e9c4e73f17283faa342e79c49ec844033a
Ravi kiran Chiruvolu <chiruvol@verizonmedia.com>
2021-08-03 11:08:58 -0700

    Test in PR

--- create_git_tag.sh
+++ create_git_tag.sh
@@ -8,8 +8,9 @@ CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
+echo Version parts $VNUM1.$VNUM2.$VNUM3
 let val=$VNUM3+1
 NEW_TAG="$VNUM1.$VNUM2.$val" 
 echo "Committing new tag $NEW_TAG"
 git tag $NEW_TAG
-git push --tags
+#git push --tags
