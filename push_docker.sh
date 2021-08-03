ada952ab0aa0ecdc39407f022e25eb8da3a6b035
mailravi3390 <mailravi3390@gmail.com>
2021-08-03 15:51:55 -0700

    Try (#26)

--- push_docker.sh
+++ push_docker.sh
@@ -1,10 +1,10 @@
 VERSION=`git describe --tags --abbrev=0`
-echo "Docker push version ${VERSION}"
+echo "Docker push version ${VERSION} Login user: $LOGIN_USERNAME"
 curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 docker build . -t opentsdb-meta
 echo "Built the image"
 docker tag opentsdb-meta:latest opentsdb/opentsdb-meta:$VERSION
-echo "Tagged the image. Will do docker login with $DOCKER_USERNAME"
-docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
+echo "Tagged the image."
+docker login --username $LOGIN_USERNAME --password $LOGIN_PASSWORD
 echo Pushing to opentsdb/opentsdb-meta:$VERSION
 #docker image push opentsdb/opentsdb-meta:$VERSION
