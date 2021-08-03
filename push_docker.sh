abaace79b8981193603dbc57b23c6101c747152d
Ravi kiran Chiruvolu <chiruvol@verizonmedia.com>
2021-08-03 15:17:38 -0700

    verify in shell

--- push_docker.sh
+++ push_docker.sh
@@ -4,7 +4,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 docker build . -t opentsdb-meta
 echo "Built the image"
 docker tag opentsdb-meta:latest opentsdb/opentsdb-meta:$VERSION
-echo "Tagged the image"
+echo "Tagged the image. Will do docker login with $DOCKER_USERNAME"
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 echo Pushing to opentsdb/opentsdb-meta:$VERSION
 #docker image push opentsdb/opentsdb-meta:$VERSION
